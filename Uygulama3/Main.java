45cc98ddb024589aa5c7e628108659526ecfd11c
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-08-24 21:01:46 +0300

    Encapsulation

---
+++ Uygulama3/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+    public static void main(String[] args) {
+        
+        Book book1 = new Book("Harry Poter", 459);
+        Book book2 = new Book("Lord Of The Rings", -662);
+        
+        System.out.println(book1.getPageNumber());
+    }
+}
