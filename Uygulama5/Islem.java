179755a263322754bd5347278c61b67b88985b8e
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-08-25 13:45:48 +0300

    polymorphism

---
+++ Uygulama5/Islem.java
@@ -0,0 +1,9 @@
+public class Islem{
+
+    static void girisYapanlar (Calisan[] loginUsers){
+
+        for(Calisan c :loginUsers){
+            c.giris();
+        }
+    }
+}
--- Uygulama5/Main.java
+++ Uygulama5/Main.java
@@ -2,12 +2,16 @@ public class Main{
     public static void main(String[] args) {
 
         Akademisyen akademisyen1= new Akademisyen("ayşe", "5431231212", "a@b.com", "CENG", "Ogretim Gorevlisi");
-        akademisyen1.derseGir();
+        //akademisyen1.derseGir();
 
         LabAsistani labCalisan1 = new LabAsistani("muti", "432", "a@b.co", "CENG", "ASA", "02:00");
+        Memur memur1= new Memur("ahmet ", "32134","a@d.cd", "oi", 12);
+        //labCalisan1.giris("12:00");
+        //labCalisan1.giris();
 
-        labCalisan1.giris("12:00");
-        labCalisan1.giris();
+        Calisan [] calisanlar ={akademisyen1,memur1,labCalisan1}; 
+
+        Islem.girisYapanlar(calisanlar);
     
     }
 }
